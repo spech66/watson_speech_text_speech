07789f8f8d267bb980101ab4a385e9376b43661a
Sebastian Pech <spech@spech.de>
2017-03-05 17:38:52 +0100

    Config

---
+++ config.js
@@ -0,0 +1,18 @@
+// The attention word to wake up the robot.
+exports.attentionWord ='watson';
+
+// You can change the voice of the robot to your favorite voice.
+exports.voice = 'en-US_MichaelVoice';
+// Some of the available options are:
+// en-US_AllisonVoice
+// en-US_LisaVoice
+// en-US_MichaelVoice (the default)
+
+// Credentials for Watson Speech to Text service
+
+exports.STTUsername = 'xxx-xxx-xxx-xxx-xxx' ;
+exports.STTPassword = 'xxxxxx' ;
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = 'xxx-xxx-xxx-xxx-xxx' ;
+exports.TTSPassword = 'xxxxxx' ; 
